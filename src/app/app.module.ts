b25921f214679db2ae27ee6f96d61a546afeb8de
CristopherJmnz <crisjimenez19@gmail.com>
2023-11-20 00:45:14 +0100

    original commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http'
+import { routing,appRoutingProvider } from './app.routing';
+
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    appRoutingProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/app.routing.ts
@@ -0,0 +1,9 @@
+import{ Routes, RouterModule} from '@angular/router'
+import { ModuleWithProviders } from "@angular/core";
+import { HomeComponent } from './components/home/home.component';
+
+const appRoutes:Routes=[
+    {path:"", component:HomeComponent},
+]
+export const appRoutingProvider:any[]=[];
+export const routing:ModuleWithProviders<any>=RouterModule.forRoot(appRoutes);
